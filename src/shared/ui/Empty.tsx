0aa1eaa252f194d1e7459f5efa1bf64dbab40de6
TheFrozeKing <frozefama@mail.ru>
2026-09-03 14:27:59 +0300

    Removed redundant modules

--- src/shared/ui/Empty.tsx
+++
@@ -1,8 +0,0 @@
-interface IEmpty {
-  width?: string;
-  height?: string;
-}
-
-export default function Empty({ width, height }: IEmpty) {
-  return <div style={{ width: width, height: height }}></div>;
-}
